7ce7f30a7da865d29d423d1e9557151713bdcfb0
abdulbasit <abasitkk92@gmail.com>
2025-01-05 06:02:59 +0500

    search flight detail section added

--- app/_features/flight/SearchFlightDetail.jsx
+++ app/_features/flight/SearchFlightDetail.jsx
@@ -3,21 +3,22 @@ import Image from "next/image";
 import planeGrayIcon from "@/public/plane-gray-icon.svg";
 
 export default function SearchFlightDetail() {
-  return null;
-  // <div className="py-8">
-  //   <div>
-  //     <div>
-  //       <p>
-  //         <Image src={planeGrayIcon} alt="depart plane icon" />
-  //         <span>From</span>
-  //       </p>
-  //       <p>Jakarta</p>
-  //       <p>
-  //         <span>Sun, 20 Aug</span>
-  //         <span>&bull;</span>
-  //         <span>08:35</span>
-  //       </p>
-  //     </div>
-  //   </div>
-  // </div>
+  return (
+    <div className="py-8">
+      <div>
+        <div>
+          <p>
+            <Image src={planeGrayIcon} alt="depart plane icon" />
+            <span>From</span>
+          </p>
+          <p>Jakarta</p>
+          <p>
+            <span>Sun, 20 Aug</span>
+            <span>&bull;</span>
+            <span>08:35</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
