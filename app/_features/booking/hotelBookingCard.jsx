723e51c22fa234df7a52df342f762add6f7cbbde
Shahbaz <rshahbaz.masood@gmail.com>
2025-04-18 11:17:53 +0500

    booking card settings

--- app/_features/booking/hotelBookingCard.jsx
+++ app/_features/booking/hotelBookingCard.jsx
@@ -8,7 +8,7 @@ export const HotelBookingCard = ({ searchParams, hotel, room }) => {
     <>
       <div className="bg-white rounded-[20px] mt-5 h-[240px] p-4">
         <div className="grid grid-cols-7 h-full w-full gap-5">
-          <div className="flex flex-col content-center col-span-3 h-full col-start-1 col-end-4">
+          <div className="flex flex-col items-center col-span-3 h-full col-start-1 col-end-4">
             <Image
               src="/trending-image-1.jpg"
               className="h-[200px]"
@@ -18,28 +18,40 @@ export const HotelBookingCard = ({ searchParams, hotel, room }) => {
               height={10}
             />
           </div>
-          <div className="col-span-2 col-start-4 col-end-6">
-            <div className="flex flex-col justify-center w-full h-full gap-5">
-              <div className="flex flex-col content-start w-full">
-                <div className="flex items-center gap-2">
-                  <div className="flex items-center justify-center rounded-3xl bg-[#172B85] text-[#FCCD27] w-[36px] h-[36px]">
-                    <FaBuilding />
-                  </div>
-                  <div>
-                    <p className="text-xs text-[#808080]">Hotel Name</p>
-                    <p className="text-sm text-[#1A1A1A]">
-                      {hotel?.HotelInfo?.HotelName}
-                    </p>
-                  </div>
+          <div className="col-span-4 col-start-4 col-end-8 flex flex-col justify-around">
+            <div className="flex items-start gap-3">
+              <div className="flex items-center gap-2 flex-1">
+                <div className="flex items-center justify-center rounded-3xl bg-[#172B85] text-[#FCCD27] w-[36px] h-[36px]">
+                  <FaBuilding />
+                </div>
+                <div>
+                  <p className="text-xs text-[#808080]">Hotel Name</p>
+                  <p className="text-sm text-[#1A1A1A]">
+                    {hotel?.HotelInfo?.HotelName}
+                  </p>
                 </div>
               </div>
-              <div className="w-full">
+              <div className="flex justify-end flex-1">
+                <ButtonCustom className="text-white bg-[#172B85] rounded-xl">
+                  <MdOutlineDeleteForever />
+                  Remove
+                </ButtonCustom>
+              </div>
+            </div>
+            <div className="flex items-start gap-3">
+              <div className="flex-1">
                 <p className="text-xs text-[#808080]">Hotel Location</p>
                 <p className="text-sm text-[#1A1A1A]">
                   {searchParams?.selectedHotelAddress}
                 </p>
               </div>
-              <div className="w-full">
+              <div className="flex-1">
+                <p className="text-xs text-[#808080]">Room Type</p>
+                <p className="text-sm text-[#1A1A1A]">{room?.RoomType}</p>
+              </div>
+            </div>
+            <div className="flex items-start gap-3">
+              <div className="flex-1">
                 <p className="text-xs text-[#808080]">Check In</p>
                 <p className="text-sm text-[#1A1A1A]">
                   {new Date(searchParams?.checkIn).toLocaleDateString("en-GB", {
@@ -51,31 +63,18 @@ export const HotelBookingCard = ({ searchParams, hotel, room }) => {
                 </p>
                 <p className="text-sm text-[#1A1A1A]">00:10pm</p>
               </div>
-            </div>
-          </div>
-          <div className="col-span-2 col-start-6 col-end-8">
-            <div className="flex flex-col justify-center w-full h-full gap-5">
-              <div className="flex justify-end">
-                <ButtonCustom className="text-white bg-[#172B85] rounded-xl">
-                  <MdOutlineDeleteForever />
-                  Remove
-                </ButtonCustom>
-              </div>
-              <div className="w-full">
-                <p className="text-xs text-[#808080]">Room Type</p>
-                <p className="text-sm text-[#1A1A1A]">
-                  {room?.RoomType}
-                </p>
-              </div>
-              <div className="w-full">
+              <div className="flex-1">
                 <p className="text-xs text-[#808080]">Check In</p>
                 <p className="text-sm text-[#1A1A1A]">
-                  {new Date(searchParams?.checkOut).toLocaleDateString("en-GB", {
-                    weekday: "short",
-                    day: "2-digit",
-                    month: "short",
-                    year: "numeric",
-                  })}
+                  {new Date(searchParams?.checkOut).toLocaleDateString(
+                    "en-GB",
+                    {
+                      weekday: "short",
+                      day: "2-digit",
+                      month: "short",
+                      year: "numeric",
+                    }
+                  )}
                 </p>
                 <p className="text-sm text-[#1A1A1A]">00:10pm</p>
               </div>
